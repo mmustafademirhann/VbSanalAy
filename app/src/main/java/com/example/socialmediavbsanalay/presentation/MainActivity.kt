8147f00d748e48935da55dda3ad6208ac54062f1
mmustafademirhann <mmustafademirhann@gmail.com>
2024-08-22 14:55:26 +0300

    4.

---
+++ app/src/main/java/com/example/socialmediavbsanalay/presentation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.socialmediavbsanalay.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.socialmediavbsanalay.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        //ı deleted
+
+    }
+}
