f644e16529f73bcc3c86ee39e3d21c1ff0754f2a
st900642 <mmustafademirhann@gmail.com>
2024-08-22 11:50:08 +0300

    ı delted

--- app/src/main/java/com/example/socialmediavbsanalay/MainActivity.kt
+++ app/src/main/java/com/example/socialmediavbsanalay/MainActivity.kt
@@ -7,9 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("gjk")
-        print("dsfd")
-        print("3.d")
-        print("4.d")
+        //ı deleted
     }
 }
