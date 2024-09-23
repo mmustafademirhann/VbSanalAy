b20f91bc698ad6b0261699a3c94e634af8146844
st900642 <mmustafademirhann@gmail.com>
2024-09-23 03:36:53 +0300

    burada bazı değişiklikler yaptım

--- app/src/main/java/com/example/socialmediavbsanalay/domain/model/Post.kt
+++ app/src/main/java/com/example/socialmediavbsanalay/domain/model/Post.kt
@@ -9,4 +9,4 @@ data class Post(
     val imageResId: String, // Resource ID of the image for the story
     val username: String     // Username of the user associated with the story
      // URL of the user's profile photo
-):Serializable
+)//kldfnnklda
